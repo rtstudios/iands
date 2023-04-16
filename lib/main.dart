63674671fc7cfcc0a0b0f8964482bebaadb9b5a5
Rifat Tarafder <tarafderw@gmail.com>
2023-04-16 11:30:58 -0400

    99% done with the initial screen

--- lib/main.dart
+++ lib/main.dart
@@ -52,29 +52,134 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  height: MediaQuery.of(context).size.height * 0.7,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(13),
-                    border: Border.all(
-                      width: 1.5,
-                      color: const Color.fromARGB(87, 255, 255, 255)
-                    ),
-                    gradient: const LinearGradient(
-                      colors: [Color.fromARGB(143, 255, 255, 255), Color.fromARGB(8, 255, 255, 255)],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                    ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                height: MediaQuery.of(context).size.height * 0.7,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(13),
+                  border: Border.all(
+                    width: 1.5,
+                    color: const Color.fromARGB(87, 255, 255, 255)
                   ),
-                  child: Column(
-                    children: const [
-                      Text("data")
-                    ],
+                  gradient: const LinearGradient(
+                    colors: [Color.fromARGB(200, 255, 255, 255), Color.fromARGB(100, 255, 255, 255)],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
                   ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(4),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(13),
+                        child: const Image(
+                          width: double.infinity,
+                          height: 300,
+                          fit: BoxFit.fitHeight,
+                          image: AssetImage("lib/assets/logo.jpg")
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: const [
+                          Text(
+                            "Welcome to the IANDS",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontFamily: "Futura"
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0, 0, 0, 12),
+                            child: Text(
+                              "Conference Companion",
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontFamily: "Futura",
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            "This app allows you to see the conference schedule and bookmark sessions you are interested in attending.", 
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontFamily: "Futura"
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.all(4),
+                      child: Container(
+                        width: double.infinity,
+                        height: 75,
+                        decoration: BoxDecoration(
+                          color: Colors.white60,
+                          borderRadius: BorderRadius.circular(13),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Flexible(
+                                flex: 1,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(13),
+                                  child: MaterialButton(
+                                    height: double.infinity,
+                                    minWidth: double.infinity,
+                                    color: const Color.fromARGB(255, 240, 240, 240),
+                                    child: const Text(
+                                      "Create Account",
+                                      style: TextStyle(
+                                        fontFamily: "Futura",
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 16
+                                      ),
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                ),
+                              ),
+                              Flexible(
+                                flex: 1,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(13),
+                                  child: MaterialButton(
+                                    height: double.infinity,
+                                    minWidth: double.infinity,
+                                    child: const Text(
+                                      "Sign In",
+                                      style: TextStyle(
+                                        fontFamily: "Futura",
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 16
+                                      ),
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
